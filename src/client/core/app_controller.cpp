f37e7ea92523d7b76a7cb45bb39c7c590a8e0fb6
2024051604119FengChunlin <1356962534@qq.com>
2026-04-14 15:53:19 +0800

    修复了gitignore文件里的忽略规则，修复了别人拉取下来可以正常构建编译

---
+++ src/client/core/app_controller.cpp
@@ -0,0 +1,42 @@
+/**
+* @file    app_controller.cpp
+* @date    2026-05-24
+* @author  GY
+* @brief   AppController 实现
+*
+* Change Log:
+* [v0.1] GY   2026-05-24
+* * Stage 0：实现 applicationName / applicationVersion / quit
+*/
+
+#include "app_controller.h"
+
+#include <QCoreApplication>
+#include <QDebug>
+
+AppController::AppController(QObject *parent)
+    : QObject{parent}
+{
+}
+
+AppController *AppController::create(QQmlEngine *engine, QJSEngine *)
+{
+    Q_UNUSED(engine);
+    return new AppController{};
+}
+
+QString AppController::applicationName() const
+{
+    return QCoreApplication::applicationName();
+}
+
+QString AppController::applicationVersion() const
+{
+    return QCoreApplication::applicationVersion();
+}
+
+void AppController::quit()
+{
+    qDebug() << "AppController::quit invoked from QML";
+    QCoreApplication::quit();
+}
