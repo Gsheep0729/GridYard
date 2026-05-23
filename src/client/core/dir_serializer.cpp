01c3a8ef5dd4c17ee6e2033fcac7f11df547dded
2024051604085GaoYang <2697438381@qq.com>
2026-05-23 10:37:14 +0800

    Stage 4.1：DirSerializer 目录序列化工具，版本号 v0.4.1

---
+++ src/client/core/dir_serializer.cpp
@@ -0,0 +1,91 @@
+/**
+* @file    dir_serializer.cpp
+* @date    2026-06-04
+* @author  GY
+* @brief   DirSerializer 实现
+*
+* Change Log:
+* [v0.1] GY   2026-06-04
+* * Stage 4：初始实现
+*/
+
+#include "dir_serializer.h"
+
+#include <QCryptographicHash>
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+
+namespace gy {
+
+QList<FileItem> DirSerializer::serialize(const QString &path)
+{
+    QList<FileItem> result;
+    QFileInfo info(path);
+
+    if (!info.exists()) {
+        return result;
+    }
+
+    if (info.isFile()) {
+        FileItem item;
+        item.relativePath = info.fileName();
+        item.sizeBytes = info.size();
+        item.sha256 = computeSha256(path);
+        result.append(item);
+    } else if (info.isDir()) {
+        traverseDir(path, QString(), result);
+    }
+
+    return result;
+}
+
+QString DirSerializer::computeSha256(const QString &filePath)
+{
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadOnly)) {
+        return QString();
+    }
+
+    QCryptographicHash hash(QCryptographicHash::Sha256);
+    if (!hash.addData(&file)) {
+        return QString();
+    }
+
+    return hash.result().toHex();
+}
+
+void DirSerializer::traverseDir(const QString &basePath,
+                                const QString &currentPath,
+                                QList<FileItem> &result)
+{
+    QDir dir(basePath + (currentPath.isEmpty() ? QString() : "/" + currentPath));
+    QFileInfoList entries = dir.entryInfoList(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
+
+    // 先记录空目录（如果当前目录为空）
+    if (entries.isEmpty() && !currentPath.isEmpty()) {
+        FileItem item;
+        item.relativePath = currentPath + "/";
+        item.sizeBytes = 0;
+        item.sha256 = QString();
+        result.append(item);
+    }
+
+    for (const QFileInfo &entry : entries) {
+        QString relPath = currentPath.isEmpty()
+            ? entry.fileName()
+            : currentPath + "/" + entry.fileName();
+
+        if (entry.isFile()) {
+            FileItem item;
+            item.relativePath = relPath;
+            item.sizeBytes = entry.size();
+            item.sha256 = computeSha256(entry.absoluteFilePath());
+            result.append(item);
+        } else if (entry.isDir()) {
+            traverseDir(basePath, relPath, result);
+        }
+    }
+}
+
+} // namespace gy
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -24,6 +24,8 @@
 * * Stage 3：版本号更新，连接体验优化
 * [v0.4] GY   2026-06-03
 * * Stage 3.10：版本号更新到 v0.3.1
+* [v0.5] GY   2026-06-04
+* * Stage 4.1：DirSerializer 实现，版本号更新到 v0.4.1
 */
 
 #include <QCommandLineParser>
@@ -38,7 +40,7 @@ int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QGuiApplication::setApplicationName("GridYard");
-    QGuiApplication::setApplicationVersion("0.3.1");
+    QGuiApplication::setApplicationVersion("0.4.1");
     QGuiApplication::setOrganizationName("CQNU-SED");
 
     // 命令行参数解析
