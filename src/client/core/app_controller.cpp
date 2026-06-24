aba77bc3f50a265d3f74cfb6b69ace2dfed9f6c1
2024051604085GaoYang <2697438381@qq.com>
2026-06-25 01:05:33 +0800

    新增领域持久化端口和 SQLite 迁移基础

--- src/client/core/app_controller.cpp
+++ src/client/core/app_controller.cpp
@@ -1,7 +1,7 @@
 /**
 * @file    app_controller.cpp
-* @version 4.16.5
-* @date    2026-06-24
+* @version 4.16.8
+* @date    2026-06-25
 * @author  GridYard Team
 * @brief   应用全局控制器实现
 *
@@ -9,6 +9,8 @@
 * TransferSessionManager，启动 P2P 服务器并初始化传输会话管理器。
 *
 * Change Log:
+* [v4.16.8] GY   2026-06-25
+* * 集中管理本地历史数据库与数据库任务线程
 * [v4.16.5] FengChunlin   2026-06-24
 * * 创建并初始化在线聊天管理器
 * [v4.7.1] GY   2026-06-06
@@ -20,14 +22,18 @@
 */
 
 #include "app_controller.h"
+#include "application_paths.h"
+#include "database_worker.h"
 #include "chat_manager.h"
 #include "config_manager.h"
 #include "discovery_service.h"
 #include "p2p_server.h"
 #include "transfer_session_manager.h"
+#include "sqlite_database_proxy.h"
 
 #include <QCoreApplication>
 #include <QDebug>
+#include <QThread>
 
 // 构造函数：创建并组装所有核心模块
 AppController::AppController(QObject *parent)
@@ -37,7 +43,23 @@ AppController::AppController(QObject *parent)
     , _p2pServer{new P2pServer{_config, this}}
     , _transfer{new TransferSessionManager{this}}
     , _chat{new ChatManager{this}}
+    , _storage{std::make_unique<SqliteDatabaseProxy>()}
+    , _storageThread{new QThread{this}}
+    , _storageWorker{new DatabaseWorker{_storage.get()}}
 {
+    QString storageError;
+    // 历史库失败时仅记录降级状态，不阻断已验收的 P2P 主链路
+    if (!_storage->initialize(ApplicationPaths::databaseDir() + "/gridyard-history.sqlite", &storageError)) {
+        qWarning() << "[Storage] 本地历史不可用:" << storageError;
+    }
+
+    // Worker 不设置 parent，移动后由线程结束信号安全回收
+    _storageWorker->moveToThread(_storageThread);
+    // 线程退出时在各自安全的事件上下文中释放 Worker 和线程对象
+    connect(_storageThread, &QThread::finished, _storageWorker, &QObject::deleteLater);
+    connect(_storageThread, &QThread::finished, _storageThread, &QObject::deleteLater);
+    _storageThread->start();
+
     // 启动 P2P 服务器
     _p2pServer->start();
 
@@ -46,6 +68,16 @@ AppController::AppController(QObject *parent)
 
     // 初始化聊天管理器
     _chat->init(_config, _discovery, _p2pServer);
+}
+
+// 析构函数
+AppController::~AppController()
+{
+    if (_storageThread && _storageThread->isRunning()) {
+        // 先结束任务事件循环，再由 finished 信号删除 Worker
+        _storageThread->quit();
+        _storageThread->wait();
+    }
 }
 
 // QML_SINGLETON 工厂方法，引擎调用
